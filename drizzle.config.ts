3bd009a088bc139ac594da48e6958d0547a5b849
xionghailong <xionghailongk@gmail.com>
2026-03-27 10:46:24 +0800

    将 drizzle 后端更改为 PG

--- drizzle.config.ts
+++ drizzle.config.ts
@@ -1,4 +1,7 @@
 import { defineConfig } from "drizzle-kit";
+import { config } from "dotenv";
+
+config({ path: ".env.development.local" });
 
 export default defineConfig({
   schema: "./src/db/schema.ts",
--- src/db/index.ts
+++ src/db/index.ts
@@ -1,14 +1,15 @@
-import { neon } from "@neondatabase/serverless";
-import { drizzle } from "drizzle-orm/neon-http";
+import { drizzle } from "drizzle-orm/postgres-js";
+import postgres from "postgres";
 import * as schema from "./schema";
 
 const getDrizzle = () => {
   const databaseUrl = process.env.DATABASE_URL;
+
   if (!databaseUrl) {
     throw new Error("DATABASE_URL environment variable is not set");
   }
-  const sql = neon(databaseUrl);
-  return drizzle(sql, { schema });
+  const client = postgres(databaseUrl);
+  return drizzle(client, { schema });
 };
 
 export const getDb = () => getDrizzle();
